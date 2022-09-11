90a4c1ca8a7facf5f8ae89063dfd56a8dd8aee1a
Fallenz <beijuspsn@163.com>
2022-09-11 15:39:33 +0800

    build default framework

---
+++ pull-consumer/src/main/java/com/flz/rm/sb/pullconsumer/PullConsumerApplication.java
@@ -0,0 +1,11 @@
+package com.flz.rm.sb.pullconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PullConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PullConsumerApplication.class, args);
+    }
+}
