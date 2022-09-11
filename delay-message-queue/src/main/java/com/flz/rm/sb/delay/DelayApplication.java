1ec0a1b1ea492a302192a1b67b5cee2f7a4958f0
Fallenz <beijuspsn@163.com>
2022-09-11 20:37:56 +0800

    build framework of delay message queue demo

---
+++ delay-message-queue/src/main/java/com/flz/rm/sb/delay/DelayApplication.java
@@ -0,0 +1,12 @@
+package com.flz.rm.sb.delay;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.flz")
+public class DelayApplication {
+    public static void main(String[] args) {
+
+    }
+}
